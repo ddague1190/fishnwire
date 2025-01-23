4e0227e0588094547b6b7d14e918f511d8be7f79
Darryl <77030053+ddague1190@users.noreply.github.com>
2025-01-23 10:44:56 -0500

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -151,7 +151,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'fishnstik1',
+        'NAME': 'fishnwire',
         'USER': 'postgres',
         'PASSWORD': os.getenv('POSTGRES_PASS'),
         'HOST': os.getenv('DATABASE_ENDPOINT'),
